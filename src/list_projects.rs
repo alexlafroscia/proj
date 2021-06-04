9f7fe0f3dffaa7679baec7144fc87ebc1732f1c9
Alex LaFroscia <alex@lafroscia.com>
2021-06-03 22:48:29 -0400

    refactor: extract module for `list_projects`

---
+++ src/list_projects.rs
@@ -0,0 +1,34 @@
+use std::fs::{self, DirEntry};
+use std::path::{Path, PathBuf};
+
+fn is_repo(entry: &DirEntry) -> bool {
+    let name = entry.path();
+    let mut git_dir = name.clone();
+    git_dir.push(Path::new(".git"));
+
+    git_dir.exists()
+}
+
+pub fn list_projects(path: PathBuf) -> Vec<DirEntry> {
+    fs::read_dir(path)
+        .expect("Could not read directory")
+        .flat_map(|wrapped_entry| {
+            let entry = wrapped_entry.expect("Could not read entry");
+
+            if !entry
+                .file_type()
+                .expect("Could not determine file type")
+                .is_dir()
+            {
+                return vec![];
+            }
+
+            match is_repo(&entry) {
+                // if this is a repo, add it to our result
+                true => vec![entry],
+                // otherwise, descend into this directory
+                false => list_projects(entry.path()),
+            }
+        })
+        .collect::<Vec<DirEntry>>()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,9 @@
+mod list_projects;
+
 use std::env;
-use std::fs::{self, DirEntry};
 use std::io::{self, Write};
-use std::path::{Path, PathBuf};
 
-fn is_repo(entry: &DirEntry) -> bool {
-    let name = entry.path();
-    let mut git_dir = name.clone();
-    git_dir.push(Path::new(".git"));
-
-    git_dir.exists()
-}
-
-fn list_projects(path: PathBuf) -> Vec<DirEntry> {
-    fs::read_dir(path)
-        .expect("Could not read directory")
-        .flat_map(|wrapped_entry| {
-            let entry = wrapped_entry.expect("Could not read entry");
-
-            if !entry
-                .file_type()
-                .expect("Could not determine file type")
-                .is_dir()
-            {
-                return vec![];
-            }
-
-            match is_repo(&entry) {
-                // if this is a repo, add it to our result
-                true => vec![entry],
-                // otherwise, descend into this directory
-                false => list_projects(entry.path()),
-            }
-        })
-        .collect::<Vec<DirEntry>>()
-}
+use list_projects::*;
 
 fn main() {
     let stdout = io::stdout();
